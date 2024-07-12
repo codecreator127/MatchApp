99c396731d1de7d9831f81eedc31f534704f4f05
VoxelFS <oli444@aucklanduni.ac.nz>
2024-07-13 11:19:23 +1200

    Added more things to information component

--- src/components/Information.tsx
+++ src/components/Information.tsx
@@ -1,6 +1,33 @@
 import React from "react";
 
 function Information() {
+
+    const divs = [
+        {
+            name: "Plant Name",
+            description: "Cactus"
+        },
+        {
+            name: "Plant Age",
+            description: "13 Years"
+        },
+        {
+            name: "Plant Type",
+            description: "Cactaceae"
+        },
+        {
+            name: "Plant Needs",
+            description: "Sunlight, water"
+        },
+        {
+            name: "Plant Description",
+            description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`
+        }
+    ]
+
     return (
         <div className="flex flex-col overflow-auto">
             <div className="flex flex-col items-center justify-center bg-gray-800">
@@ -10,38 +37,16 @@ function Information() {
                             INFORMATION
                         </h1>
                     </div>
-                    <div className="mb-4">
-                        <label className="block text-gray-700 text-sm font-bold mb-2">
-                            Name:
-                        </label>
-                        <p className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight">
-                            Plant Name
-                        </p>
-                    </div>
-                    <div className="mb-4">
-                        <label className="block text-gray-700 text-sm font-bold mb-2">
-                            Age:
-                        </label>
-                        <p className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight">
-                            Plant Age
-                        </p>
-                    </div>
-                    <div className="mb-4">
-                        <label className="block text-gray-700 text-sm font-bold mb-2">
-                            Plant Type:
-                        </label>
-                        <p className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight">
-                            Plant Type
-                        </p>
-                    </div>
-                    <div className="mb-4">
-                        <label className="block text-gray-700 text-sm font-bold mb-2">
-                            Description:
-                        </label>
-                        <p className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                        </p>
-                    </div>
+                    {divs.map((div, index) => (
+                        <div className="mb-4" key={index}>
+                            <label className="block text-gray-700 text-sm font-bold mb-2">
+                                {div.name}:
+                            </label>
+                            <p className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight">
+                                {div.description}
+                            </p>
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
