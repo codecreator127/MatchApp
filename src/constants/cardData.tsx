020e4c6a0e07b3591eb135ebe4eb35ebd0e57083
tonylxm <126369686+tonylxm@users.noreply.github.com>
2024-07-13 20:04:44 +1200

    merge fixes

--- src/constants/cardData.tsx
+++ src/constants/cardData.tsx
@@ -1,36 +1,49 @@
-import Image from "../../public/test_image.jpeg";
-
 // Data for each card
 export const cardData = [
   {
     title: "Item 1",
+    age: 13,
+    type: "Desert",
+    needs: "Sunlight, water",
     description: "Description 1",
-    imgSrc: Image.src,
+    imgSrc: "",
     imgAlt: "Item 1",
   },
   {
     title: "Item 2",
+    age: 10,
+    type: "Rainforest",
+    needs: "Sunlight, water",
     description: "Description 2",
-    imgSrc: Image.src,
+    imgSrc: "",
     imgAlt: "Item 2",
   },
   {
     title: "Item 3",
+    age: 3,
+    type: "House",
+    needs: "Sunlight, water",
     description: "Description 3",
-    imgSrc: Image.src,
+    imgSrc: "",
     imgAlt: "Item 3",
   },
   {
     title: "Item 4",
+    age: 1,
+    type: "Forest",
+    needs: "Sunlight, water",
     description: "Description 4",
-    imgSrc: Image.src,
+    imgSrc: "",
     imgAlt: "Item 4",
   },
   {
     title: "No more items",
+    age: 0,
+    type: "Desert",
+    needs: "Sunlight, water",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-    imgSrc: Image.src,
+    imgSrc: "",
     imgAlt: "No more items",
   },
 ];
