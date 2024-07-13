79e1db2d42bde164bb4ab5bdca884bcf798c75c2
VoxelFS <oli444@aucklanduni.ac.nz>
2024-07-13 20:17:18 +1200

    temp order design

--- src/components/Orders.tsx
+++ src/components/Orders.tsx
@@ -1,4 +1,8 @@
+import { Box, Button, Card, CardActions, CardContent, CardHeader, CardMedia, IconButton, Stack } from '@mui/material';
 import React, { useState } from 'react';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import HandshakeIcon from '@mui/icons-material/Handshake';
+import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
 
 const Orders: React.FC = () => {
   const [activeDiv, setActiveDiv] = useState<number | null>(null);
@@ -14,10 +18,6 @@ const Orders: React.FC = () => {
       action: () => alert('Only appear if there is one available in area')
     },
     {
-      name: 'Claim',
-      action: () => alert('Only appear if someone has put one up')
-    },
-    {
       name: 'Sponsor',
       action: () => alert('Redirect to sponsor page?')
     }
@@ -29,22 +29,33 @@ const Orders: React.FC = () => {
   };
 
   return (
-    <div className="flex flex-col w-full h-full overflow-auto space-y-4 p-4">
-      {divs.map((div, index) => (
-        <div
-          key={index}
-          onClick={() => handleClick(index)}
-          className={`
-            w-full h-[100px] flex justify-center items-center cursor-pointer
-            transition-colors duration-300 rounded-lg
-            ${activeDiv === index ? 'bg-gray-300' : 'bg-gray-100'}
-            hover:bg-gray-200
-          `}
-        >
-          {div.name}
-        </div>
-      ))}
-    </div>
+    <>
+      <Card style={{ maxHeight: 1500 }}>
+        <CardHeader title={`You've matched with ${"Snake Plant"}!`} subheader="You can now buy, trade or sponsor this plant" />
+        <CardMedia component="img" style={{ borderRadius: "50%" }} image="https://media.post.rvohealth.io/wp-content/uploads/2022/01/snake-plant-detail-732x549-thumbnail-732x549.jpg" alt="Plant" />
+        <CardContent>
+          <Stack spacing={2}>
+            {divs.map((div, index) => (
+              <Button variant='contained' key={index} sx={{ backgroundColor: "#8cb1a1", "&:hover": {backgroundColor: "#a8c4b8"}, height: 50, fontSize: 18, borderRadius: 100, textTransform: "lowercase" }}>
+                {div.name} Plant
+              </Button>
+            ))}
+          </Stack>
+        </CardContent>
+        {/*<CardActions disableSpacing>
+          <IconButton aria-label='Buy Plant'>
+            <AddShoppingCartIcon sx={{ height: 30, width: 30 }}/>
+          </IconButton>
+          <IconButton aria-label='Trade Plant'>
+            <HandshakeIcon sx={{ height: 30, width: 30 }} />
+          </IconButton>
+          <IconButton aria-label='Sponsor Plant'>
+            <AttachMoneyIcon sx={{ height: 30, width: 30 }} />
+          </IconButton>
+        </CardActions>
+        */}
+      </Card>
+    </>
   );
 };
 
