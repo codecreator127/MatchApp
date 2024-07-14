b4e30e1f3bab6d402d6ce8a872a87ee540efb951
VoxelFS <oli444@aucklanduni.ac.nz>
2024-07-14 13:35:45 +1200

    Background preference change

--- src/app/preferences/page.tsx
+++ src/app/preferences/page.tsx
@@ -6,7 +6,7 @@ import Preferences from "@/components/Preference";
 export default function Preference() {
   return (
     <>
-      <div className="h-screen w-screen">
+      <div className="h-screen w-screen" style={{ backgroundColor: "#c5d8cf" }}>
         <div className="h-full w-2/5 mx-auto flex justify-center items-center">
           <Preferences />
         </div>
