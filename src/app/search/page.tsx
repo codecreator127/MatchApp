8a43dd75924b101609b2e9291157c5cce60cd88a
John <codecreator127@gmail.com>
2024-07-14 12:01:18 +1200

    search gpt set up, parsing function not working

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -6,80 +6,6 @@ import ResultCard from "@/components/ResultCard";
 import { Box, Stack, Typography } from "@mui/material";
 import { searchImages } from "../api/pixabay/route";
 
-function setError(arg0: string) {
-  throw new Error("Function not implemented.");
-}
-
-function setLoading(arg0: boolean) {
-  throw new Error("Function not implemented.");
-}
-
-async function gptCall(preferences: string) {
-  try {
-    const response = await fetch("/api/openai", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ preferences }),
-    });
-
-    const result = await response.json();
-    return result;
-  } catch (error) {
-    console.error("Error:", error);
-    setError("Error: Could not fetch response");
-  } finally {
-    setLoading(false);
-  }
-}
-
-function parsePlants(text: string): {
-  imgUrl: any;
-  name: string;
-  plantType: string;
-  caringGuide: string;
-}[] {
-  const plantEntries = text.split(/\d+\.\s+/); // Split text based on numbers followed by a dot and optional whitespace
-
-  const plants = plantEntries.map((entry) => {
-    const lines = entry.trim().split("\n");
-    const plantInfo: {
-      name: string;
-      plantType: string;
-      caringGuide: string;
-      imgUrl: string;
-    } = { name: "", plantType: "", caringGuide: "", imgUrl: "" };
-
-    lines.forEach((line) => {
-      if (line.startsWith("Name:")) {
-        plantInfo.name = line.split(": ")[1].trim();
-      } else if (line.startsWith("Plant Type:")) {
-        plantInfo.plantType = line.split(": ")[1].trim();
-      } else if (line.startsWith("Caring Guide:")) {
-        plantInfo.caringGuide = line.split(": ")[1].trim();
-      }
-    });
-
-    return plantInfo;
-  });
-
-  return plants.filter(
-    (plant) =>
-      plant.name !== "" && plant.plantType !== "" && plant.caringGuide !== ""
-  );
-}
-
-async function generatePlants(scenario: string) {
-  const output = await gptCall(scenario);
-  const plantMap = parsePlants(output);
-  return plantMap;
-}
-
-const replaceSpaces = (query: string) => {
-  return query.replace(/\s/g, "+");
-};
-
 export default function Home() {
   const [search, setSearch] = useState("");
 
@@ -109,7 +35,113 @@ export default function Home() {
     },
   ]);
 
+  async function gptCall(preferences: string) {
+    try {
+      const response = await fetch("/api/openai", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ preferences }),
+      });
+
+      const result = await response.json();
+      return result;
+    } catch (error) {
+      console.error("Error:", error);
+      // setError("Error: Could not fetch response");
+    } finally {
+      // setLoading(false);
+    }
+  }
+
+  function parsePlants(text: string): {
+    imgUrl: any;
+    name: string;
+    plantType: string;
+    caringGuide: string;
+    description: string;
+  }[] {
+    const plantEntries = text.split(/\d+\.\s+/); // Split text based on numbers followed by a dot and optional whitespace
+
+    const plants = plantEntries.map((entry) => {
+      const lines = entry.trim().split("\n");
+      const plantInfo: {
+        name: string;
+        plantType: string;
+        caringGuide: string;
+        imgUrl: string;
+        description: string;
+      } = {
+        name: "",
+        plantType: "",
+        caringGuide: "",
+        imgUrl: "",
+        description: "",
+      };
+
+      lines.forEach((line) => {
+        if (line.startsWith("Name:")) {
+          plantInfo.name = line.split(": ")[1].trim();
+        } else if (line.startsWith("Plant Type:")) {
+          plantInfo.plantType = line.split(": ")[1].trim();
+        } else if (line.startsWith("Caring Guide:")) {
+          plantInfo.caringGuide = line.split(": ")[1].trim();
+        } else if (line.startsWith("Description:")) {
+          plantInfo.description = line.split(": ")[1].trim();
+        }
+      });
+
+      return plantInfo;
+    });
+
+    return plants.filter(
+      (plant) =>
+        plant.name !== "" && plant.plantType !== "" && plant.caringGuide !== ""
+    );
+  }
+
+  async function generatePlants(scenario: string) {
+    const output = await gptCall(scenario);
+
+    console.log(output);
+    const plantMap = parsePlants(output);
+    console.log(plantMap);
+    // const plantMap = [
+    //   {
+    //     name: "Heartleaf Philodendron",
+    //     plantType: "Indoor vine",
+    //     caringGuide:
+    //       "Bright, indirect light. Water when top inch of soil is dry.",
+    //     imgUrl:
+    //       "https://pixabay.com/get/gb299173c7ed50e9e591cca48609a1d6e434a62f9b6d57611fdb846614cc50b388d7dfd2579b0d10c8bedc21df2d2024f0256f306f9626fc65965391a03fe834e_640.jpg",
+    //   },
+    //   {
+    //     name: "String of Hearts",
+    //     plantType: "Hanging succulent",
+    //     caringGuide:
+    //       "Bright, indirect light. Allow soil to dry between waterings.",
+    //     imgUrl:
+    //       "https://pixabay.com/get/g681cd8794628b71cc0874e72cb29de52faba62f8b1c59ab67fef472ca1fbd5fb945c629567af96841bbc3aaee34294af888646483caae8ebdd3498385b2a294c_640.jpg",
+    //   },
+    //   {
+    //     name: "Anthurium",
+    //     plantType: "Flowering houseplant",
+    //     caringGuide:
+    //       "Bright, indirect light. Keep soil consistently moist but not soggy.",
+    //     imgUrl:
+    //       "https://pixabay.com/get/ga83b26b7a693c4849212e7dab3e94c9e145bdc7de298c4c3e4cb542af80b4493c7a442a8d6063a1d2ab6542fc847a88afe7ea5c93b45d790ef542ab8f9243388_640.jpg",
+    //   },
+    // ];
+    return plantMap;
+  }
+
+  const replaceSpaces = (query: string) => {
+    return query.replace(/\s/g, "+");
+  };
+
   useEffect(() => {
+    console.log(search);
     async function fetchData() {
       try {
         const data = await generatePlants(search);
@@ -139,68 +171,80 @@ export default function Home() {
     fetchData();
   }, [search]);
 
-  const plants = [
-    {
-      id: 1,
-      name: "Plant 1",
-      species: "Species 1",
-      summary: "This is plant 1",
-      description:
-        "Plant 1 is really cool and native to place. It is a great plant to have in your garden.",
-      wateringFrequency: "Once a week",
-      maintenance: "Low",
-      image: "https://via.placeholder.com/150",
-      imageAlt: "Plant 1",
-    },
-    {
-      id: 2,
-      name: "Plant 2",
-      species: "Species 2",
-      summary: "This is plant 2",
-      description:
-        "Plant 2 is really cool and native to place. It is a great plant to have in your garden.",
-      wateringFrequency: "Once a week",
-      maintenance: "Low",
-      image: "https://via.placeholder.com/150",
-      imageAlt: "Plant 2",
-    },
-    {
-      id: 3,
-      name: "Plant 3",
-      species: "Species 3",
-      summary: "This is plant 3",
-      description:
-        "Plant 3 is really cool and native to place. It is a great plant to have in your garden.",
-      wateringFrequency: "Once a week",
-      maintenance: "Low",
-      image: "https://via.placeholder.com/150",
-      imageAlt: "Plant 3",
-    },
-    {
-      id: 4,
-      name: "Plant 4",
-      species: "Species 4",
-      summary: "This is plant 4",
-      description:
-        "Plant 4 is really cool and native to place. It is a great plant to have in your garden.",
-      wateringFrequency: "Once a week",
-      maintenance: "Low",
-      image: "https://via.placeholder.com/150",
-      imageAlt: "Plant 4",
-    },
-    {
-      id: 5,
-      name: "Plant 5",
-      species: "Species 5",
-      summary: "This is plant 5",
-      description:
-        "Plant 5 is really cool and native to place. It is a great plant to have in your garden.",
-      wateringFrequency: "Once a week",
-      maintenance: "Low",
-      image: "https://via.placeholder.com/150",
-      imageAlt: "Plant 5",
-    },
-  ];
+  // const plants = [
+  //   {
+  //     id: 1,
+  //     name: "Plant 1",
+  //     species: "Species 1",
+  //     summary: "This is plant 1",
+  //     description:
+  //       "Plant 1 is really cool and native to place. It is a great plant to have in your garden.",
+  //     wateringFrequency: "Once a week",
+  //     maintenance: "Low",
+  //     image: "https://via.placeholder.com/150",
+  //     imageAlt: "Plant 1",
+  //   },
+  //   {
+  //     id: 2,
+  //     name: "Plant 2",
+  //     species: "Species 2",
+  //     summary: "This is plant 2",
+  //     description:
+  //       "Plant 2 is really cool and native to place. It is a great plant to have in your garden.",
+  //     wateringFrequency: "Once a week",
+  //     maintenance: "Low",
+  //     image: "https://via.placeholder.com/150",
+  //     imageAlt: "Plant 2",
+  //   },
+  //   {
+  //     id: 3,
+  //     name: "Plant 3",
+  //     species: "Species 3",
+  //     summary: "This is plant 3",
+  //     description:
+  //       "Plant 3 is really cool and native to place. It is a great plant to have in your garden.",
+  //     wateringFrequency: "Once a week",
+  //     maintenance: "Low",
+  //     image: "https://via.placeholder.com/150",
+  //     imageAlt: "Plant 3",
+  //   },
+  //   {
+  //     id: 4,
+  //     name: "Plant 4",
+  //     species: "Species 4",
+  //     summary: "This is plant 4",
+  //     description:
+  //       "Plant 4 is really cool and native to place. It is a great plant to have in your garden.",
+  //     wateringFrequency: "Once a week",
+  //     maintenance: "Low",
+  //     image: "https://via.placeholder.com/150",
+  //     imageAlt: "Plant 4",
+  //   },
+  //   {
+  //     id: 5,
+  //     name: "Plant 5",
+  //     species: "Species 5",
+  //     summary: "This is plant 5",
+  //     description:
+  //       "Plant 5 is really cool and native to place. It is a great plant to have in your garden.",
+  //     wateringFrequency: "Once a week",
+  //     maintenance: "Low",
+  //     image: "https://via.placeholder.com/150",
+  //     imageAlt: "Plant 5",
+  //   },
+  // ];
+
+  const plants = cardData.map((plant, index) => ({
+    id: index + 1,
+    name: plant.name,
+    species: plant.plantType, // Assuming plantType is species
+    summary: `This is ${plant.name}`,
+    description: `${plant.caringGuide}`,
+    wateringFrequency: "Once a week", // Assuming a default value
+    maintenance: "Low", // Assuming a default value
+    image: plant.imgUrl,
+    imageAlt: plant.name,
+  }));
 
   return (
     <>
